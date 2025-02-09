30f6bf8b0f1e3020f4f9889836fef7c0f4be27c3
DhanuCode01 <lakshanshanaka327@gmail.com>
2025-02-09 05:58:25 +0530

    Version 0.0.1👌👍

--- Router/PaymentRouter.js
+++ Router/PaymentRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addPayment , updatePayment , getHalfpaid, getUnpaid} from "../Controller/PaymentController.js";
+import { addPayment , updatePayment , getHalfpaid} from "../Controller/PaymentController.js";
 
 
 const paymentRouter=express.Router();
@@ -7,7 +7,7 @@ const paymentRouter=express.Router();
 paymentRouter.post("/add",addPayment);
 paymentRouter.put("/add/:email",updatePayment);
 paymentRouter.get("/",getHalfpaid);
-paymentRouter.get("/user",getUnpaid);
+
 
 
 export default paymentRouter;
